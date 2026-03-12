2b8f545e5616b016adff41cfd5ad4cea7dc16c29
Kyumin_Kim <kkm010412@gmail.com>
2026-03-12 23:20:15 +0900

    day5 2

--- week3/basic/02_divide_conquer.py
+++ week3/basic/02_divide_conquer.py
@@ -36,19 +36,23 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
     
+    if left == right:
+        return arr[left]
     # TODO: 중간 지점 계산
-    pass
-    
-    # TODO: 왼쪽 절반의 최댓값
-    pass
+    else:
+        mid = (left+right)//2
+        # TODO: 왼쪽 절반의 최댓값
+        left_big = find_max_divide_conquer(arr,left, mid)
+            
+        # TODO: 오른쪽 절반의 최댓값
+        right_big = find_max_divide_conquer(arr,mid + 1, right)
         
-    # TODO: 오른쪽 절반의 최댓값
-    pass
-    
-    # TODO: 둘 중 큰 값 반환
-    pass
+        # TODO: 둘 중 큰 값 반환
+        if left_big < right_big:
+            return right_big
+        else:
+            return left_big
 
 # 테스트 케이스
 if __name__ == "__main__":
