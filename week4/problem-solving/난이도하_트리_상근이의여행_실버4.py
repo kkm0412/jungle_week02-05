8b5a941730831c2b64b2b09bef4604fb82fc713f
Kyumin_Kim <kkm010412@gmail.com>
2026-03-20 22:51:26 +0900

    day 9 2

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,38 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+
+#그래프 문제임
+#이게 왜 트리 문제야;;;;
+#
+from collections import deque
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    planes = {}
+    for n in range(N+1):
+        planes[n] = []
+    for m in range(M):
+        a, b = map(int, input().split())
+        planes[a].append(b)
+        planes[b].append(a)
+    queue = deque([1])
+    visited = []
+    count = 0
+    while len(queue) != 0:
+        x = queue.popleft()
+
+        can_go = planes[x]
+        for y in can_go:
+            if y not in visited:
+                count += 1
+                queue.append(y)
+                visited.append(y)
+    print(count)
+    # print(N-1)
+#처리과정
+#아무곳에서 시작하면
+#bfs 방식으로 전부 읽어와서 몇번인지 세기
+
+
--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,60 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+# 리프 늘리는 방법: 기본적으로 리프가 2개 생기므로, 기본적으로 좌측줄기에 잎을 몰아주다가
+# m-2개 만큼 리프를 1층부터 우측 줄기에 붙이면 됨 
+
+# class Tree():
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+# n, m = map(int, input().split())
+
+# root = Tree(0)
+
+# def append(node, n, is_right):    #node: 현재 줄기, n = 잎의 순번
+#     if n != 0 and node.right is None and is_right == True:
+#         node.right = Tree(n)
+#     elif node.left is not None:
+#         append(node.left, n, is_right)
+#     else:
+#         node.left = Tree(n)
+# def show(node, queue):
+#     if node.left is not None:
+#         queue.append((node.data, node.left.data))
+#     if node.right is not None:
+#         queue.append((node.data, node.right.data))
+#         if node.left is not None:
+#             show(node.left, queue)
+#         if node.right is not None:
+#             show(node.left, queue)
+    
+    
+# leaf_count = m - 2
+
+# for i in range(1, n-1):
+#     if leaf_count >0:
+#         append(root, i, True)
+#         leaf_count -= 1
+#     else:
+#         append(root, i, False)
+
+# queue = []
+# show(root, queue)
+# for x in queue:
+#     if x[1] is not None:
+#         print(x[0], x[1])
+
+
+n, m = map(int, input().split())
+
+trees = []
+for i in range(n-m):
+    trees.append((i, i+1))
+
+for x in range(1, m):
+    trees.append((n-m, n-m+ x))
+
+for tree in trees:
+    print(tree[0], tree[1])
